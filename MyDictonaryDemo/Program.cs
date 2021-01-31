bbc4bac778af5500a70433557424948cd6821718
Serdar-Y <72358742+Serdar-Y@users.noreply.github.com>
2021-02-01 00:10:41 +0300

    Add project files.

---
+++ MyDictonaryDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictonaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> enTr = new MyDictionary<string, string>();
+            enTr.Add("pencil", "Kalem");
+            enTr.Add("Book", "Kitap");
+            enTr.Add("Plate", "Tabak");
+            enTr.Add("Table", "Masa");
+            enTr.Add("School", "Okul");
+            enTr.Add("Car", "Araba");
+
+            enTr.Write();
+        }
+    }
+}
